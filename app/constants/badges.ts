74fb897c7c070429745286688de63213999f11e8
markuu-exe <marklawrenceamatong@gmail.com>
2026-05-15 03:28:44 +0800

    import badge assets and then replaced old assets for quest achievements

--- app/constants/badges.ts
+++ app/constants/badges.ts
@@ -9,9 +9,15 @@ export type BadgeDef = {
   criteria: string;
 };
 
-const BADGE_SHIELD = require('../../assets/ProfileAssets/BadgeShield.png');
-const BADGE_MEDAL = require('../../assets/ProfileAssets/BadgeMedal.png');
-const BADGE_HAT = require('../../assets/ProfileAssets/BadgeHat.png');
+const Badge_Initiatior = require('../../assets/BadgeAssets/Badge_Initiatior.png');
+const Badge_Adventurer = require('../../assets/BadgeAssets/Badge_Adventurer.png');
+const Badge_Patron = require('../../assets/BadgeAssets/Badge_Patron.png');
+const Badge_Disguise = require('../../assets/BadgeAssets/Badge_Disguise.png');
+const Badge_Paragon = require('../../assets/BadgeAssets/Badge_Paragon.png');
+const Badge_Veteran = require('../../assets/BadgeAssets/Badge_Veteran.png');
+const Badge_NightOwl = require('../../assets/BadgeAssets/Badge_NightOwl.png');
+const Badge_Hero = require('../../assets/BadgeAssets/Badge_Hero.png');
+const Badge_Dev = require('../../assets/BadgeAssets/Badge_Dev.png');
 
 export const BADGES: BadgeDef[] = [
   {
@@ -19,7 +25,7 @@ export const BADGES: BadgeDef[] = [
     name: 'The Initiator',
     description: 'Awarded for posting your very first quest.',
     category: 'quest',
-    icon: BADGE_SHIELD,
+    icon: Badge_Initiatior,
     criteria: 'Post 1 quest.',
   },
   {
@@ -27,7 +33,7 @@ export const BADGES: BadgeDef[] = [
     name: 'The Adventurer',
     description: 'Awarded for successfully completing your first quest.',
     category: 'quest',
-    icon: BADGE_MEDAL,
+    icon: Badge_Adventurer,
     criteria: 'Complete 1 quest.',
   },
   {
@@ -35,7 +41,7 @@ export const BADGES: BadgeDef[] = [
     name: 'Prolific Patron',
     description: 'Awarded for posting a total of 10 quests.',
     category: 'quest',
-    icon: BADGE_HAT,
+    icon: Badge_Patron,
     criteria: 'Post 10 quests.',
   },
 ];
